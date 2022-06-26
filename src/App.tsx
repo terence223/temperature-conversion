3583cad9cb51420c70d21ca6ff7ccba6e5369006
terence223 <terence223@mac.com>
2022-06-26 19:14:59 +0800

    feat : basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
+import { Layout } from 'antd';
+import styled from 'styled-components';
 
-import logo from './logo.svg';
+import TemperatureConversion from './pages/TemperatureConversion';
 
 import './App.css';
 
-function App() {
+const { Header, Content } = Layout;
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{ height: '100vh' }}>
+        <Header className="header" />
+        <Content style={{ padding: '50px' }}>
+          <TemperatureConversion />
+        </Content>
+      </Layout>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/TemperatureConversion.tsx
@@ -0,0 +1,5 @@
+const TemperatureConversion = () => {
+  return <div>test</div>;
+};
+
+export default TemperatureConversion;
