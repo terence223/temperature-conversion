b4ec0dcc5d8edf806c94df7a020b1154baaa045f
terence223 <terence223@mac.com>
2022-06-28 11:42:29 +0800

    fix : decimal typescript

--- src/utils/temperature.ts
+++ src/utils/temperature.ts
@@ -1,5 +1,4 @@
 // use reliable math library to prevent javscript math precision problem
-// but it doesn't support typescript, please allow me to ignore for now
 import { Decimal } from 'decimal.js';
 
 export const ABSOLUTE_ZERO_IN_CELCIUS = -273.15;
@@ -8,13 +7,14 @@ export const ABSOLUTE_ZERO_IN_FARENHEIT = -459.67;
 export const TEMPERATURE_REGEX = /^(-)?\d+(\.\d{1,2})?$/;
 
 export const covertCelciusToFarenheit = (cel: number): number => {
-  //@ts-ignore
-  return (Math.round(new Decimal(cel).mul(9).div(5).add(32).mul(100)) /
-    100) as number;
+  return (Math.round(
+    new Decimal(cel).mul(9).div(5).add(32).mul(100).toNumber()
+  ) / 100) as number;
 };
 
 export const covertFarenheitToCelcius = (far: number): number => {
-  //@ts-ignore
-  return (Math.round(new Decimal(far).minus(32).mul(5).div(9).mul(100)) /
-    100) as number;
+  return (
+    Math.round(new Decimal(far).minus(32).mul(5).div(9).mul(100).toNumber()) /
+    100
+  );
 };
